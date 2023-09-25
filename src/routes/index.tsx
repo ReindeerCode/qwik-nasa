5ec2f14df0c2309cb8cd95ee319f61a63c01864e
MJRudolph <ReindeerCode@gmail.com>
2023-09-25 19:02:28 -0400

    working on passing Private API key from server to fetch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead, RequestEvent } from "@builder.io/qwik-city";
+import { ExplanationSelector } from "~/components/nasaImageComponents/pic_selector";
 
-const nasaImageURL = "https://api.nasa.gov/planetary/apod?api_key=";
-// //const DB_NASA_IMAGE_PRIVATE_KEY = {import.meta.env.NASA_IMAGE_PRIVATE_KEY}
+export const onGet = (requestEvent: RequestEvent) => {
+  return requestEvent.env.get("DB_NASA_IMAGE_PRIVATE_KEY");
+  //console.log(requestEvent.env.get("DB_NASA_IMAGE_PRIVATE_KEY"));
+};
 
 
 
 export default component$(() => {
-  const onGet = (requestEvent: RequestEvent) => {
-    console.log(requestEvent.env.get("DB_NASA_IMAGE_PRIVATE_KEY"));
-  };
-
   return (
     <>
-      <button
-        onClick$={() => console.log(import.meta.env.VITE_NASA_IMAGE_PUBLIC_KEY)}
-      >
-        test button
-      </button>
+      <ExplanationSelector />
     </>
   );
 });
