8d9b9e72d8610c6abba7b7d5739dd045b388542c
MJRudolph <ReindeerCode@gmail.com>
2023-10-23 17:59:49 -0400

    cleaning up files. determined it is not a good idea to use Resource on API calls that have private keys but instead on public api calls like a google maps call

--- src/routes/nasaImage/index.tsx
+++ src/routes/nasaImage/index.tsx
@@ -1,44 +1,46 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useResource$, useStore } from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
-import { ExplanationSelector } from "~/components/nasaImageComponents/nasaData_selector";
-
 
 export const useNasaAPI = routeLoader$(async (requestEvent) => {
   const URL = import.meta.env.VITE_NASA_IMAGE_PUBLIC_KEY;
   const nasaKey = requestEvent.env.get("DB_NASA_IMAGE_PRIVATE_KEY");
   const res = await fetch(`${URL}${nasaKey}`);
   const nasaData = await res.json();
-  // console.log(nasaData)
   return nasaData;
 });
 
-
-
 export default component$(() => {
   const response = useNasaAPI().value;
-  // console.log(response.title);
-  const testResponse = {
+
+  const responseTable = useStore({
+    date: response.date,
+    explanation: response.explanation,
+    hdurl: response.hdurl,
+    media_type: response.media_type,
+    service_version: response.service_version,
     title: response.title,
     url: response.url,
-    date: response.date
-  };
-  console.log(testResponse);
-  const testResponse2 = useStore({ title: response.title, url: response.url, date:response.date });
-  console.table(testResponse2);
+  });
 
   return (
     <center>
       <h1>
         Title:{" "}
         <p>
-          <b>{testResponse2.title}</b>
+          <b>{responseTable.title}</b>
         </p>
       </h1>
       <h4>
-        Image:{" "}
+        Image / HD Image:{" "}
         <p>
           <img
-            src={response.url}
+            src={responseTable.url}
+            alt="Nasa Img of The Day"
+            width="500"
+            height="600"
+          ></img>
+          <img
+            src={responseTable.hdurl}
             alt="Nasa Img of The Day"
             width="500"
             height="600"
@@ -48,16 +50,15 @@ export default component$(() => {
       <h4>
         Date:{" "}
         <p>
-          <b>{response.date}</b>
+          <b>{responseTable.date}</b>
         </p>
       </h4>
       <h4>
         Explanation:{" "}
         <p>
-          <b>{response.explanation}</b>
+          <b>{responseTable.explanation}</b>
         </p>
       </h4>
-      <ExplanationSelector />
     </center>
   );
 });
@@ -71,3 +72,6 @@ export const head: DocumentHead = {
     },
   ],
 };
+function async() {
+  throw new Error("Function not implemented.");
+}
